5b89a8b1a065923304f866a4568522a41a914e25
Aman <kuaman2000@gmail.com>
2023-07-21 07:50:43 +0530

    First Commit

---
+++ src/main/java/mypack/App.java
@@ -0,0 +1,20 @@
+package mypack;
+
+
+public class App 
+{
+	
+	public static boolean isEqual(int a,int b)
+	{
+		return (a == b)? true : false;		
+	}
+	
+		
+    public static void main( String[] args )
+    {
+        System.out.println(App.isEqual(9,9));
+        System.out.println(App.isEqual(-3,-3));
+        System.out.println(App.isEqual(9,10));
+        System.out.println(App.isEqual(3,4));
+    }
+}
